f1150f4c6ab528ef57936fda624627de661f6cd1
harddap <144228835+harddap@users.noreply.github.com>
2023-10-02 14:04:14 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,100 @@
+#a = int(input())
+
+#print(a + 1)
+#print(a + 2)
+#print()
+
+
+
+#x = int(input())
+
+#print(x * x * x)
+#print(6 * x * x)
+
+
+
+#a = int(input())
+#b = int(input())
+
+#print(3 * (a + b) * (a + b) * (a + b) + 275 * b * b - 127 * a - 41)
+
+
+
+#c = int(input())
+#print(c)
+#print(c - 1)
+#print(c + 1)
+
+
+
+#x1 = int(input())
+#x2 = int(input())
+#x3 = int(input())
+#x4 = int(input())
+#print(x1 + x2 + x3 + x4)
+
+
+
+#a_1 =  int(input())
+#d =  int(input())
+#n = int(input())
+#print(a_1 = a_1 + d * (n - 1))
+
+
+
+#n = int(input())
+#print('    _-_       ' * n)
+#print('  ( o O )     ' * n)
+#print(' /   V   \    ' * n)
+#print('/(   _   )\   ' * n)
+#print('   ^^  ^^     ' * n)
+
+
+
+#nun = int(input())
+#a = num % 10
+#b = (num % 100) // 10
+#c = num //100
+#print ('сумма =' , c + b + a)
+#print ('произведение =' , c * b * a)
+
+
+
+#n = int(input())
+#k = int(input())
+#x = k // n
+#y = k - (x * n)
+#print(x)
+# #print(y)
+
+
+
+#n = int(input())
+#print(n//2 + n%2)
+
+
+
+#m = int(input())
+# #h = m // 60
+#s = m % 60
+# #print(m, "мин - это", h, "час", s, "минут.")
+
+
+
+#num = int(input())
+#if num < 10:
+#print(num)
+#else:
+#print(num // 10 % 10)
+
+
+
+#n = int(input())
+#hours = n % (60 * 24) // 60
+#minutes = n % 60
+#print(hours, minutes)
+
+
+
+#n = int(input())
+#print(1 - n)
